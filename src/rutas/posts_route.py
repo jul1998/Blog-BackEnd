33b1b8aaa21beb5c87cc6460958e7e11f0efd2b4
jul1998 <jguevara321@gmail.com>
2023-01-26 16:17:19 -0600

    modified route to render html

--- src/rutas/posts_route.py
+++ src/rutas/posts_route.py
@@ -3,6 +3,8 @@ from ..main import request, jsonify, app, bcrypt, create_access_token, get_jwt_i
 from ..db import db
 from ..modelos import User, BlockedList, Posts
 from flask import Flask, url_for, redirect
+from htmlmin import minify
+
 from datetime import datetime, timezone, time
 import json
 from ..utils import APIException
@@ -10,6 +12,7 @@ from datetime import datetime
 
 
 @app.route("/create_posts", methods=["GET","POST"])
+@jwt_required()
 def create_post():
     body = request.get_json()
     print(body)
@@ -19,13 +22,14 @@ def create_post():
         post_img = body["post_img"]
 
     try:
-
-        new_post = Posts(title=body["title"],post_img=post_img, subtitle=body["subtitle"], content=body["content"], created_at = now, author_id=body["author_id"] )
+        minified_html = minify(body["content"], remove_empty_space=True)
+        print(minified_html)
+        new_post = Posts(title=body["title"],post_img=post_img, subtitle=body["subtitle"], content=minified_html, created_at = now, author_id=body["author_id"] )
         db.session.add(new_post)
         db.session.commit()
         return jsonify({"message":"Post was created"})
     except Exception as err:
-        raise APIException(f"Something went wrong when creating post: {err}")
+        raise APIException(f"Something went wrong when creating post: {err}", status_code=400)
 
    
 
@@ -36,6 +40,7 @@ def show_posts():
     return jsonify(all_posts_list)
 
 @app.route("/post/<int:post_id>")
+@jwt_required()
 def show_post_by_id(post_id):
     
     post = Posts.query.filter_by(id=post_id).first()
